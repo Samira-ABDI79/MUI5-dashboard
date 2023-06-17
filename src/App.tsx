fd6c31ab17124602b50f5d6d71b0eb8ff4c2733d
samira abdi <samiraabdiurmia@gmail.com>
2023-06-17 23:23:56 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Input } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import Home from "./pages/home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/dashboard" element={<Input />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        marginLeft: "17%",
+        marginTop: "5.5rem",
+      }}
+    >
+      Home
+    </Box>
+  );
+}
